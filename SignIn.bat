1f2b5bbe4d35c29b44e0c7c360a47f43bbad9b11
ray <ray6.ray61027@gmail.com>
2019-10-24 22:12:47 +0800

    first commit

---
+++ SignIn.bat
@@ -0,0 +1,5 @@
+cd C:\Users\ray\PycharmProjects\playground
+call C:\Users\ray\PycharmProjects\playground\venv\Scripts\activate.bat
+call python C:\Users\ray\PycharmProjects\playground\AutoRegister\SignIn.py
+REM pause
+
---
+++ SignIn.py
@@ -0,0 +1,4 @@
+from AutoRegister import AutoRegister
+
+if __name__ == '__main__':
+    AutoRegister("signin")
